d5a45cb11f753748e05c45324d7142a720758cca
Martin Olson <martinguyolson@gmail.com>
2023-05-31 09:35:53 +1000

    more backend building... added update to readme

--- backend/techtest/src/main/java/com/pulsi/Person.java
+++ backend/techtest/src/main/java/com/pulsi/Person.java
@@ -1,24 +1,33 @@
 package com.pulsi;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "persons")
 public class Person implements Serializable {
 
+  @Column
   String name;
+
+  @Column
   String address;
 
-  public Person(Long id, String name, String address) {
+  @Column
+  Date createdAt;
+
+  public Person(Long id, String name, String address, Date createdAt) {
     super();
     this.id = id;
     this.name = name;
     this.address = address;
+    this.createdAt = createdAt;
   }
 
   public Person() {}
--- backend/techtest/src/main/java/com/pulsi/PersonService.java
+++ backend/techtest/src/main/java/com/pulsi/PersonService.java
@@ -1,8 +1,18 @@
 package com.pulsi;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class PersonService {}
+public class PersonService {
+
+  @Autowired
+  private PersonRepository personRepository;
+
+  //we need this one to do update by ID
+  @Autowired
+  private ModelMapper mapper;
+}
